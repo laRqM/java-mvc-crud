b74de6abd70cd6cd0392138736a2b373ca0276c6
Luis Ronquillo <127155272+laRqM@users.noreply.github.com>
2023-10-01 22:20:12 -0600

    Create BusquedaForm.java

---
+++ Vista/BusquedaForm.java
@@ -0,0 +1,14 @@
+package Vista;
+
+import javax.swing.*;
+
+public class BusquedaForm extends JFrame {
+    public BusquedaForm() {
+
+        setTitle("Búsqueda de Datos");
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+    }
+}
